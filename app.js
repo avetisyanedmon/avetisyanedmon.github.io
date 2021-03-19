ec90616c8aadd87dff108d1d6f4f7f55b34cbe73
edmonavet1syan <80975176+edmonavet1syan@users.noreply.github.com>
2021-03-19 21:19:18 +0400

    Test
    Test

--- app.js
+++ app.js
@@ -1,101 +1,71 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+$(function() {
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeBottom = new Image();
-
-bird.src = "img/bird.png";
-bg.src = "img/bg.png";
-fg.src = "img/fg.png";
-pipeUp.src = "img/Up.png";
-pipeBottom.src = "img/Bottom.png";
-
-var fly = new Audio();
-var score_audio = new Audio();
-var fail = new Audio();
-
-fly.src = "audio/fly.mp3";
-score_audio.src = "audio/score.mp3";
-fail.src = "audio/fail.wav";
-
-var gap = 90;
-
-//Onclick!!!
-
-document.addEventListener("keydown", moveUp);
-
-function moveUp(){
-    yPos -= 25;
-    fly.play();
-};
-
-// Blocks!!!
-
-var pipe = [];
-
-pipe[0] = {
-    x: cvs.width,
-    y: 0
-}
-
-var score = 0;
-// Position of bird 
-
-var xPos = 10;
-var yPos = 150;
-var grav = 1.5;
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
 
 
-function draw(){
-    ctx.drawImage(bg, 0, 0);
+    $(window).on("scroll load resize", function() {
+        
+        scrollPos = $(this).scrollTop();
 
-    for(var i = 0; i < pipe.length; i++){
-        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-       
-        pipe[i].x--;
-
-        if(pipe[i].x == 125){
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeUp.height) 
-                - pipeUp.height
-            })
+        if( scrollPos > introH ){
+            header.addClass("fixed");
+        }else{
+            header.removeClass("fixed");
         }
 
-        if(xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x
-            + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height
-                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-                   return fail.play(),
-                   alert("Fail!!!"),
-                   location.reload();
+    
+
+    });
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let nav = $("#nav");
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show")
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 60
+        },1500)
+
+    });
 
 
-                    
-                }
-                if(pipe[i].x == 5){
-                    score++;
-                    score_audio.play();
-                }
-            
 
-    }
+    /* Nav Toggle*/
 
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    
+    $("#navToggle").on("click", function(event){
+        event.preventDefault();
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, xPos, yPos)
+        nav.toggleClass("show")
 
-    yPos += grav;
-
-    ctx.fillStyle = "#000";
-    ctx.font = "24px Verdana";
-    ctx.fillText = ("Score:" + score, 10, cvs.height -20 )
+    });
+    
 
 
-    requestAnimationFrame(draw)
-};
+    /* Slider */
 
-pipeBottom.onload = draw;
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true,
+    
+      });
+    
+    
+   
+
+}); 
